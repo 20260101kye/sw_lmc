b7b9160f2ee3cc3d40497db8f183ea717d48ab4b
20260101kye <dpdms060317@kookmin.ac.kr>
2026-03-18 23:32:32 +0900

    5543

---
+++ 5543.py
@@ -0,0 +1,8 @@
+상덕버거 = int(input("상덕버거 값을 입력하세요> "))
+중덕버거 = int(input("중덕버거 값을 입력하세요> "))
+하덕버거 = int(input("하덕버거 값을 입력하세요> "))
+burger = [상덕버거, 중덕버거, 하덕버거]
+콜라 = int(input("콜라 값을 입력하세요> "))
+사이다 = int(input("사이다 값을 입력하세요> "))
+beverege = [콜라, 사이다]
+print(min(burger)+min(beverege)-50)
--- 요일계산.py
+++ 요일계산.py
@@ -1,4 +1,4 @@
 days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"]
 after = int(input("며칠 후가 궁금하신가요? > "))
 result = days[after % 7]
-print(result)
+print(result)a
