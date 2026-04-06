7af5bb3b5b96c63a95988fa6088ec451ea16b10a
20260101kye <dpdms060317@kookmin.ac.kr>
2026-04-06 22:47:21 +0900

    백준 8958

---
+++ 8958.py
@@ -0,0 +1,13 @@
+T = int(input())
+for _ in range(T):
+    s = input()
+    score = 0
+    count = 0
+    for ch in s:
+        if ch == 'O':
+            count += 1
+            score = score + count
+        else:
+            count = 0
+        
+    print(score)
